746ddd3d610c25b6834daf57ad2b25db33132443
Daxanius <balder.huybreghs@gmail.com>
2022-09-07 17:39:36 +0200

    Fix elytra rendering cape textures

---
+++ src/main/java/com/modkiller1001/nocapes/mixins/ElytraLayerMixin.java
@@ -0,0 +1,45 @@
+package com.modkiller1001.nocapes.mixins;
+
+import com.modkiller1001.nocapes.NoCapes;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ElytraLayer.class)
+public class ElytraLayerMixin {
+    @Final
+    @Shadow
+    private static ResourceLocation WINGS_LOCATION;
+
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
+            at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/player/AbstractClientPlayer;getElytraTextureLocation()Lnet/minecraft/resources/ResourceLocation;"))
+    private ResourceLocation getElytraTextureLocation(AbstractClientPlayer abstractClientPlayer) {
+        String capeUrl = NoCapes.getCapeURL(abstractClientPlayer.getGameProfile());
+        if (!NoCapes.shouldRenderCape(capeUrl)) {
+            return WINGS_LOCATION;
+        }
+
+        return abstractClientPlayer.getElytraTextureLocation();
+    }
+
+    @Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/player/AbstractClientPlayer;getCloakTextureLocation()Lnet/minecraft/resources/ResourceLocation;"))
+    private ResourceLocation getCloakTextureLocation(AbstractClientPlayer abstractClientPlayer) {
+        String capeUrl = NoCapes.getCapeURL(abstractClientPlayer.getGameProfile());
+        if (!NoCapes.shouldRenderCape(capeUrl)) {
+            return WINGS_LOCATION;
+        }
+
+        return abstractClientPlayer.getCloakTextureLocation();
+    }
+}
